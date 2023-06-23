fdd3d38f9bf97a0cf056a4ce540118b1ed1bbc23
JonR205 <gohan205@gmail.com>
2023-06-23 14:02:09 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Text from "./components/text";
+
+function App() {
+  return (
+    <div className="App">
+      <Text></Text>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/text.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Text() {
+  return <div className="text"> Hello World</div>;
+}
+
+export default Text;
