89ce79c1048a4c4dcf75195c33b21d51e45e273b
JonR205 <gohan205@gmail.com>
2023-06-27 20:28:53 -0500

    activated onclick

--- src/components/submitButton.tsx
+++ src/components/submitButton.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "../App.css";
 
-const Button = () => {
-  //({ onClick }) => {
+const apiGet = () => {
+  fetch("https://catfact.ninja/fact")
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+    });
+};
+
+function Button() {
   return (
-    <button className="lcars-element button">
-      {/* onClick={onClick}> */}
+    <button className="lcars-element button" onClick={apiGet}>
       Click me!
     </button>
   );
-};
+}
 
 export default Button;
