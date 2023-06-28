c8b720ff8a61614cc5a4cfac6a2b0847ff0f6d74
JonR205 <gohan205@gmail.com>
2023-06-27 21:30:10 -0500

    Chganged to post and added BG image

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,19 @@ import NameInput from "./components/nameInput";
 import Button from "./components/submitButton";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import background from "./images/LCARS-Excalibur.jpg";
 
 function App() {
   return (
-    <div id="container" className="App">
+    <div
+      id="container"
+      className="App"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    >
       <Header></Header>
       <Text></Text>
       <NameInput></NameInput>
