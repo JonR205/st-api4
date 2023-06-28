69a29dd00b31838e0ba65ebf3c5f808572bf3ee9
JonR205 <gohan205@gmail.com>
2023-06-27 21:31:09 -0500

    Missed

--- src/components/submitButton.tsx
+++ src/components/submitButton.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import "../App.css";
 
 const apiGet = () => {
-  fetch("https://catfact.ninja/fact")
+
+  const requestOptions = {
+    method: 'POST',
+    // headers: { 'Content-Type': 'application/json' },
+    // body: JSON.stringify({ title: 'React POST Request Example' })
+};
+
+  fetch(
+    "https://stapi.co/api/v2/rest/performer/search?title=william%20shatner&name=william%20shatner", requestOptions
+  )
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
